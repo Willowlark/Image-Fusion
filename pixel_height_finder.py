5da99a539c25b53dbb87e2b38bbe3b270bf3d5ea
Bob <seedor87@students.rowan.edu>
2016-07-21 18:17:26 -0400

    Working out bugs in pixel parsing for height investigation

--- pixel_height_finder.py
+++ pixel_height_finder.py
@@ -6,6 +6,9 @@ import numpy
 
 red = (249, 24, 0)
 
+directory = os.path.dirname(os.path.realpath(__file__))
+infile = directory + '\\Input\\OdEct.png'
+
 def color_separator(im):
     if im.getpalette():
         im = im.convert('RGB')
@@ -20,26 +23,48 @@ def color_separator(im):
             colors_dict[pix[i,j]].putpixel((i,j), pix[i,j])
     return colors_dict
 
-directory = os.path.dirname(os.path.realpath(__file__))
-infile = directory + '\\Input\\OdEct.png'
+def find_height(infile):
 
-im = Image.open(infile)
-colors_dict = color_separator(im)
-# pprint(colors_dict)
-out = colors_dict[red]
-# out.show()
+    im = Image.open(infile)
+    colors_dict = color_separator(im)
+    # pprint(colors_dict)
+    out = colors_dict[red]
+    # out.show()
 
-pixels = list(out.getdata())
-width, height = im.size
-pixels = [pixels[i * width:(i + 1) * width] for i in xrange(height)]
+    pixels = list(out.getdata())
+    width, height = im.size
+    pixels = [pixels[i * width:(i + 1) * width] for i in xrange(height)]
 
-first = next(row for row in pixels if row.__contains__(red))
-top = pixels.index(first)
-print "first @", top
+    first = next(row for row in pixels if row.__contains__(red))
+    top = pixels.index(first)
+    print "first @", top
 
-last = next(row for row in pixels[::-1] if row.__contains__(red))
-bot = pixels.index(last)
-print "last @", bot
+    last = next(row for row in pixels[::-1] if row.__contains__(red))
+    bot = pixels.index(last)
+    print "last @", bot
 
-print "Height: ", bot- top
-print "px %: ", (bot - top) / height
+    return bot- top, (bot - top) / height
+
+def pixel_write(infile):
+
+    im = Image.open(infile).convert("RGB")
+    # box = (126, 132, 158, 133)  # (126, 132, 161, 200)
+    pixels = im.load()  # create the pixel map
+
+    for i in range(127, 159+1):
+        for j in range(132, 133+1):
+            pixels[i, j] = red
+
+    # for i in range(img.size[0]):  # for every pixel:
+    #     for j in range(img.size[1]):
+    #         pixels[i, j] = (i, j, i%(j+1))  # set the colour accordingly
+
+    return im
+
+if __name__ == '__main__':
+    directory = os.path.dirname(os.path.realpath(__file__))
+    infile = directory + '\\Input\\IMG_0942.jpg'
+    out = pixel_write(infile)
+
+    # infile = directory + '\\Input\\OdEct.png'
+    print find_height(out)
