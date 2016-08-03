96b434ee9018bc76ad764e7d97f6a4a995743b65
Bill <willowlark@outlook.com>
2016-08-03 17:27:32 -0400

    Debugged, detection added to the console

--- Console.py
+++ Console.py
@@ -1,8 +1,9 @@
 import argparse
 import ImageMerge
 import PixelProcess
-import sys
+import peopledetect
 import cmd
+import re
 
 class Console(cmd.Cmd):
 
@@ -12,38 +13,41 @@ class Console(cmd.Cmd):
         self.m = ImageMerge.Merger(output)
         self.prompt = '> '
 
-    def do_merge(self, *images):
+    def do_merge(self, images):
         """Merge any number of images."""
-        self.m.merge(*images)
-    def do_testMerge(self, *images):
+        self.m.merge(images)
+    def do_testmerge(self, images):
         """Merge any number of images, then reset to before the merge."""
-        self.m.testMerge(*images)
-    def do_exportMerge(self, outfile, *images):
+        self.m.testMerge(images)
+    def do_exportmerge(self, images):
         """Merge any number of images, save to the outfile, and reset the state."""
-        self.m.exportMerge(outfile, *images)
-    def do_mergeAs(self, outfile, *images):
+        paths = self.splitPaths(images)
+        self.m.exportMerge(paths[0], *paths[1:])
+    def do_mergeas(self, images):
         """Change the outfile to a new one, then merge any number of images."""
-        self.m.mergeAs(outfile, *images)
+        paths = self.splitPaths(images)
+        self.m.mergeAs(paths[0], *paths[1:])
 
-    def do_redHighlight(self, arg):
+    def do_redhighlight(self, arg):
         """When a pixel is checked true, make it red."""
         self.m.processor.setActorCommand(PixelProcess.RedHighlightCommand())
-    def do_takeSecond(self, arg):
+    def do_takesecond(self, arg):
         """When a pixel is checked true, make it the new pixel."""
         self.m.processor.setActorCommand(PixelProcess.TakeSecondCommand())
-    def do_colorDiff(self, checknum):
+    def do_colordiff(self, checknum):
         """Check a pair of pixels as true if the color difference is less than <number>."""
         self.m.processor.setCheckCommand(PixelProcess.ColorDiffCommand())
-        self.m.processor.checkcmd.diffnum = int(checknum)
-    def do_resetCommands(self, arg):
+        if checknum: self.m.processor.checkcmd.diffnum = int(checknum)
+        else: self.m.processor.checkcmd.diffnum = 0
+    def do_resetcommands(self, arg):
         """Set the check and act commands to empty commands."""
         self.m.processor.setActorCommand(PixelProcess.PixelCommand())
         self.m.processor.setCheckCommand(PixelProcess.PixelCommand())
 
-    def do_extractRemote(self, arg):
+    def do_extractremote(self, arg):
         """Set the processor's remote to record changed pixels."""
         self.m.processor = PixelProcess.ExtractPixelRemote()
-    def do_regularRemote(self, arg):
+    def do_regularremote(self, arg):
         """Set the remote to a basic one."""
         self.m.processor = PixelProcess.PixelRemote()
 
@@ -52,13 +56,21 @@ class Console(cmd.Cmd):
           and self.m.processor.pixels is not None:
             self.groups = self.m.processor.getGroupedPixels()
 
+    def do_detect(self, images):
+        """Detect people in any number of images."""
+        paths = self.splitPaths(images)
+        peopledetect.detect(paths)
 
     def do_save(self, arg):
         """Save the image."""
         self.m.save()
+        print self.m.outfile
     def do_show(self, arg):
         """Show the image."""
         self.m.show()
+
+    def splitPaths(self, string):
+        return re.findall('([^ ][^.]*\..{3})', string)
 
 if __name__ == "__main__":
     con = Console('Output\ImFuse.jpg')
