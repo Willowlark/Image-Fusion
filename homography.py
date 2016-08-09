8aaf2a64b263afc501ef5f430561f76dac7c0b0b
Bob <seedor87@students.rowan.edu>
2016-08-09 13:47:55 -0400

    completed implementation of image merge as the tool used to find the dimensions of the object that the images are homographied against. Need to fix the result of post being returned to reflect the object, still getting false results

--- homography.py
+++ homography.py
@@ -34,39 +34,43 @@ import ImageMerge, PixelProcess
 #
 # plt.show()
 
-def homograhpy(base_img, clear_dir=None, save_first_frame=None, *test_imgs):
+def homograhpy(dest_index, base_imgs, test_imgs, save_first_frame=None):
+
     args_count = 0
 
-    im_dst = cv2.imread(base_img)
+    frames_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'gif_test', 'frames')
 
-    # TODO insert ability to capture object dimensions
-    pts_dst = np.array([[179, 196], [179, 205], [198, 196], [198, 205]])
+    im_dst = cv2.imread(base_imgs[dest_index])
+
+    pts_dst = np.array(obtain_dimensions(base_imgs[dest_index], test_imgs[dest_index]))
 
     h, status = cv2.findHomography(pts_dst, pts_dst)
     im_out = cv2.warpPerspective(im_dst, h, (im_dst.shape[1], im_dst.shape[0]))
 
     if save_first_frame is not None:
-        cv2.imwrite('gif_test/frames/frame' + str(args_count) + '.png', im_out)
+        cv2.imwrite(os.path.join(frames_dir, 'frame' + str(args_count) + '.png'), im_out)
 
-    for img_path in test_imgs:
+    for index in xrange(0, len(base_imgs)):
         args_count += 1
 
-        im_dst = cv2.imread(img_path)
+        im_src = cv2.imread(test_imgs[index])
 
-        # TODO insert ability to capture object dimensions
-        pts_dst = np.array([[179, 196], [179, 205], [198, 196], [198, 205]])
+        # PIL.Image.open(base_imgs[index]).show()
+        # PIL.Image.open(test_imgs[index]).show()
 
-        h, status = cv2.findHomography(pts_dst, pts_dst)
-        im_out = cv2.warpPerspective(im_dst, h, (im_dst.shape[1], im_dst.shape[0]))
+        pts_src = np.array(obtain_dimensions(base_imgs[index], test_imgs[index]))
 
-        cv2.imwrite('gif_test/frames/frame' + str(args_count) + '.png', im_out)
+        h, status = cv2.findHomography(pts_src, pts_dst)
+        im_out = cv2.warpPerspective(im_src, h, (im_dst.shape[1], im_dst.shape[0]))
+
+        cv2.imwrite(os.path.join(frames_dir, 'frame' + str(args_count) + '.png'), im_out)
 
     print 'file args processed', args_count
 
-    apply_names(os.path.dirname(os.path.realpath(__file__)) + '\gif_test\\frames')
+    apply_names(frames_dir)
 
     file_names = sorted(
-        ('gif_test/frames/' + fn for fn in os.listdir(os.path.dirname(os.path.realpath(__file__)) + '\gif_test\\frames') if
+        ('gif_test/frames/' + fn for fn in os.listdir(frames_dir) if
          fn.endswith('.png')))
 
     print 'frames:'
@@ -83,9 +87,18 @@ def homograhpy(base_img, clear_dir=None, save_first_frame=None, *test_imgs):
     anim.run(root)
     return
 
+def obtain_dimensions(base_file, obj_file):
+    res = apply_image_merge(base_file, obj_file)
+
+    # corners of object from merge
+    top_right = [res[0][0], res[1][0]]
+    bottom_left = [res[0][1], res[1][1]]
+    top_left = [res[0][1], res[1][0]]
+    bottom_right = [res[0][0], res[1][1]]
+
+    return top_left, top_right, bottom_right, bottom_left
+
 def apply_image_merge(base_file, obj_file):
-    im = PIL.Image.open(obj_file)
-    img_width, img_height = im.size
 
     inputs = [base_file, obj_file]
     m = ImageMerge.Merger('Output/ImF.png')
@@ -114,6 +127,9 @@ def apply_image_merge(base_file, obj_file):
     # im.show()
     im.save('Output/Only Pixels.png')
 
+    print 'res of imgs', inputs[0], inputs[1]
+    PIL.Image.open('Output/Only Pixels.png').show()
+
     m.processor.setActorCommand(PixelProcess.RedHighlightCommand())
 
     m.processor.checkcmd.diffnum = 50
@@ -122,12 +138,22 @@ def apply_image_merge(base_file, obj_file):
 
     m.save()
 
-    print "obj height px", post[0].height, "\nimage height px", img_height
+    ret = post[0]
+    return ret.x, ret.y, ret.width, ret.height
 
 def apply_names(direc):
     for fn in os.listdir(direc):
         if fn.endswith('.png'):
             text_on_image(direc + '\\' + fn)
+
+def clear_frames(direc):
+    for f in os.listdir(direc):
+        os.remove(os.path.join(direc, f))
+
+def resize(file, new_w, new_h):
+    img = PIL.Image.open(file)
+    img = img.resize((new_w, new_h), PIL.Image.ANTIALIAS)
+    img.save(file)
 
 def text_on_image(path):
     img = PIL.Image.open(path)
@@ -136,13 +162,13 @@ def text_on_image(path):
     draw.text((0, 0), ntpath.basename(path), (255, 255, 255), font=font)
     img.save(path)
 
-if __name__ == '__main__' :
+def debug_main():
 
     cur_dir = os.path.dirname(os.path.realpath(__file__))
     frames_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'gif_test', 'frames')
     inp_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Input')
 
-    apply_image_merge(os.path.join(inp_dir, 'IMG_0991.jpg'), os.path.join(inp_dir, 'IMG_0993.jpg'))
+    # apply_image_merge(os.path.join(inp_dir, 'IMG_0991.jpg'), os.path.join(inp_dir, 'IMG_0993.jpg'))
 
     im_dst = cv2.imread('Input/IMG_0993.png')
     pts_dst = np.array([[179, 196], [179, 205], [198, 196], [198, 205]])
@@ -153,12 +179,11 @@ if __name__ == '__main__' :
         cv2.imwrite(os.path.join(frames_dir, 'frame0.png'), im_out)
 
     im_src1 = cv2.imread('Input/IMG_1004.png')
-    pts_src1 = np.array([[1623,1149], [1623,1405],[2282,1149],[2282,1405]])
+    pts_src1 = np.array([[1623, 1149], [1623, 1405], [2282, 1149], [2282, 1405]])
     h, status = cv2.findHomography(pts_src1, pts_dst)
-    im_out = cv2.warpPerspective(im_src1, h, (im_dst.shape[1],im_dst.shape[0]))
+    im_out = cv2.warpPerspective(im_src1, h, (im_dst.shape[1], im_dst.shape[0]))
 
     cv2.imwrite(os.path.join(frames_dir, 'frame1.png'), im_out)
-
 
     im_src2 = cv2.imread('Input/IMG_0991.png')
     pts_src2 = np.array([[116, 155], [116, 125], [179, 155], [179, 125]])
@@ -169,7 +194,8 @@ if __name__ == '__main__' :
 
     apply_names(frames_dir)
 
-    file_names = sorted((os.path.join('gif_test', 'frames', fn) for fn in os.listdir(frames_dir) if fn.endswith('.png')))
+    file_names = sorted(
+        (os.path.join('gif_test', 'frames', fn) for fn in os.listdir(frames_dir) if fn.endswith('.png')))
     pprint(file_names)
 
     images = [PIL.Image.open(fn) for fn in file_names]
@@ -181,5 +207,21 @@ if __name__ == '__main__' :
     anim = gifPlayer(root, gif_file)
     anim.pack()
     anim.run(root)
+
+if __name__ == '__main__' :
+
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    frames_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'gif_test', 'frames')
+    inp_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Input')
+
+    bases = [os.path.join(inp_dir, 'IMG_1022.jpg'), os.path.join(inp_dir, 'IMG_1024.jpg'), os.path.join(inp_dir, 'IMG_1026.jpg')]
+    tests = [os.path.join(inp_dir, 'IMG_1021.jpg'), os.path.join(inp_dir, 'IMG_1023.jpg'), os.path.join(inp_dir, 'IMG_1025.jpg')]
+    dest = (os.path.join(inp_dir, 'IMG_1024.jpg'), os.path.join(inp_dir, 'IMG_1023.jpg'))
+
+    homograhpy(dest_index=1, base_imgs=bases, test_imgs=tests)
+
+    # clear_frames(frames_dir)
+
+    # resize(os.path.join(inp_dir, 'IMG_1004.png'), PIL.Image.open(os.path.join(inp_dir, 'IMG_0993.png')).size[0], PIL.Image.open(os.path.join(inp_dir, 'IMG_0993.png')).size[1])
 
 
