2c2173bdd54a24fa7a958b8ce04dc336cb7f9ea4
Bob <seedor87@students.rowan.edu>
2016-08-25 19:32:37 -0400

    Solved problem of image placement that is not totally dependent on pixels. The image is line abstracted and the 'lines image' is used to trace for similar pixels across the lines of the total image. Still debugging

--- zoom_staging.py
+++ zoom_staging.py
@@ -1,5 +1,5 @@
 from __future__ import division
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageDraw
 import os, cv2, sys, numpy
 
 
@@ -24,36 +24,106 @@ def generate_lines(inp, out=None):
         return im
     im.save(out)
 
-def touch_up(inp, out=None):
+def apply_border(img, points, color=(255,0,0), border=3, out=None):
 
-    if isinstance(inp, basestring):
-        img = Image.open(inp)
+    if out is None:
+        im = img.copy()
     else:
-        img = inp
+        im = img
+    draw = ImageDraw.Draw(im)
 
-    return ImageOps.colorize(img.convert('L'), (0,0,0), (255,255,255))
+    shifted = points[1:] + points[:1]
+    lis = zip(points, shifted)
+
+    for pair in lis:
+        draw.line((pair[0][0], pair[0][1], pair[1][0], pair[1][1]), fill=color, width=border)
+
+    return im
+
+def find_loc(tot_img, sub_img, col=0, row=0):
+
+    sub_pix = sub_img.load()
+    tot_pix = tot_img.load()
+
+    matches = 0
+
+    # border check
+    for i in range (sub_img.size[0]):
+        if sub_pix[i, 0] == tot_pix[i + col, 0 + row]:
+            matches += 1
+    for j in range(sub_img.size[1]):
+        if sub_pix[0, j] == tot_pix[0 + col, j + row]:
+            matches += 1
+    for i in range(sub_img.size[0]):
+        if sub_pix[i, sub_img.size[1]-1] == tot_pix[i + col, 0 + row]:
+            matches += 1
+    for j in range(sub_img.size[1]):
+        if sub_pix[sub_img.size[0]-1, j] == tot_pix[0 + col, j + row]:
+            matches += 1
+
+    # total check
+    # for i in range(sub_img.size[0]):
+    #     for j in range(sub_img.size[1]):
+    #         if sub_pix[i, j] == tot_pix[i + col, j + row]:
+    #             matches += 1
+
+    return matches
+
 
 def main(zoom_factor):
 
     inp_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Input', 'mt_mckinley.jpg')
-    out_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Output', 'result.jpg')
+    out_sub_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Output', 'result_sub.jpg')
+    out_tot_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Output', 'result_tot.jpg')
 
     # staging testing
     zoomed = get_zoomed(inp_file, zoom_factor)
-    lines_zoomed = generate_lines(zoomed)
-    lines_zoomed.save(out_file)
+    # zoomed = generate_lines(zoomed)
+    #zoomed.show()
 
     # Un-packing image to be used in overlay
-    img = generate_lines(Image.open(inp_file))
-    un_zoomed = lines_zoomed.crop((0,0, zoomed.size[0] * zoom_factor, zoomed.size[1] * zoom_factor)).resize(img.size, Image.ANTIALIAS)
+    img = Image.open(inp_file)
+    img_lines = generate_lines(img)
+    img_lines.save(out_tot_file)
+
+    un_zoomed = zoomed.crop((0,0, zoomed.size[0] * zoom_factor, zoomed.size[1] * zoom_factor)).resize(img.size, Image.ANTIALIAS)
+    # un_zoomed.show()
+
+    un_zoomed_lines = generate_lines(un_zoomed)
+    # un_zoomed_lines.show()
+
+    un_zoomed_crop = un_zoomed_lines.crop((0, 0, (un_zoomed.size[0] / zoom_factor)-1, (un_zoomed.size[1] / zoom_factor)-1))
+    # un_zoomed_crop.show()
+    un_zoomed_crop.save(out_sub_file)
+
+    # img_lines.show()
+    # un_zoomed_crop.show()
+
+    width_lim = img_lines.size[0] - un_zoomed_crop.size[0]
+    height_lim = img_lines.size[1] - un_zoomed_crop.size[1]
+
+    lis = []
+    for i in range(0, width_lim):
+        for j in range(0, height_lim):
+            res = find_loc(img_lines, un_zoomed_crop, col=i, row=j)
+            lis.append((res, i, j))
+
+    lis.sort(key=lambda tup: tup[0], reverse=True)
+    matches, x, y = lis[0][0], lis[0][1], lis[0][2]
+
+    print "Succeeded", "number of matches:", matches
 
     # overlay image for visual
-    img.paste(un_zoomed.crop((0, 0, un_zoomed.size[0] / zoom_factor, un_zoomed.size[1] / zoom_factor)), (0,0))
+    img.paste(un_zoomed.crop((0, 0, un_zoomed.size[0] / zoom_factor, un_zoomed.size[1] / zoom_factor)), (x,y))
     img.show()
-    img.save(out_file)
 
-    img = touch_up(img)
-    img.show()
+    img_lines.paste(un_zoomed_lines.crop((0, 0, un_zoomed.size[0] / zoom_factor, un_zoomed.size[1] / zoom_factor)), (x,y))
+    img_lines.show()
+
+    sub_w = un_zoomed.size[0] / zoom_factor
+    sub_h = un_zoomed.size[1] / zoom_factor
+    highlighted = apply_border(img, points=[(0, 0), (0, sub_h), (sub_w, sub_h), (sub_w, 0)], border=1)
+    highlighted.show()
 
 if __name__ == '__main__':
 
